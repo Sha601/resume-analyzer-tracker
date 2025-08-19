e2422028afbdcbd4dad2f98453d79f1217203a65
Shalini K <143390138+Sha601@users.noreply.github.com>
2025-08-19 23:45:07 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -5,8 +5,9 @@ from tracker.database import init_db
 
 init_db()
 
-app=Flask(__name__)
+app = Flask(__name__)
 app.register_blueprint(views)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
